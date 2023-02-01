7c453346d916a88e677d31e100a01157585263f3
yffjglcms <yffjglcms@gmail.com>
2023-02-01 10:42:23 +0800

    配置vuepress，支持目录

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+
+// 自动获取文件到目录结构
+const getConfig = require("vuepress-bar");
+ 
+const config = getConfig(`${__dirname}/..`);
+console.log("config.sidebar:", config.sidebar);
+ 
+module.exports = {
+  title: "sina-aio",
+  description: "first test demo",
+  dest: "dist",
+  themeConfig: {
+    locales: {
+      "/": {
+        sidebar: [...config.sidebar],
+      },
+    },
+    sidebarDepth: 3 // 设置左边目录到第几层
+  },
+ 
+ 
+  
+ 
+};
